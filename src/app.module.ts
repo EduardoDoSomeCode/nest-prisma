3a0dbbfc1190be6f2d1e69951b18fd7f09de20db
eduardo.nila <eduardo.nila@teksi.mx>
2024-07-24 22:57:18 -0600

    ft:adding configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,12 @@ import { AppService } from './app.service';
 import { UserService } from './user.service';
 import { PostService } from './post.service';
 import { PrismaService } from './prisma.service';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal:true})
+  ],
   controllers: [AppController],
   providers: [ UserService,PostService,AppService ,PrismaService],
 })
